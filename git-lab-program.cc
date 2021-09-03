9ecc121fae1aacbaf1975d127a2a83d17a54c225
Hunter Stout <hs127321@ohio.edu>
2021-09-03 16:30:35 -0400

    WOOOOOgit add git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
